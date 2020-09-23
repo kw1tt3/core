9005447e017f63ed2b7f2897a69f92e712259dbc
kw1tt3 <katharina.wittkowsky@gmail.com>
2020-09-23 08:15:04 +0200

    (feat) server: add csp-header to get protected against xss attacks

--- demo/server/server.ts
+++ demo/server/server.ts
@@ -5,7 +5,20 @@ import reinit from "./reinit";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
-let appP = getApp();
+// CSP-Header to pretect xss-attacks
+// TODO: for inline script (and style) add a nonce to script-tag and script-src (or style-tag/style-src)
+const CSPHeader = { 
+  'Content-Security-Policy': `default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self' https://fonts.googleapis.com; frame-src 'self'; frame-ancestors 'none'; report-uri 'self'; require-trusted-types-for 'script';`
+}
+
+const contentSecurityPolicyConfig = {
+  rest: {
+    drafts: CSPHeader,
+    published: CSPHeader
+  }
+}
+
+let appP = getApp({responseHeaders: contentSecurityPolicyConfig});
 
 createServer(async (req, res) => {
   try {
